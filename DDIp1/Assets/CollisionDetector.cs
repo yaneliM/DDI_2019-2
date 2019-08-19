69a27980d3490d7cdc351e35d150f25c529a5d60
YMendias <yaneli.mendias@uabc.edu.mx>
2019-08-18 18:20:42 -0700

    Practica 0

---
+++ DDIp1/Assets/CollisionDetector.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Inicia Juego");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+      
+        if (collision.relativeVelocity.magnitude > 2)
+            Debug.Log("Colision detectada con "+ collision.gameObject.name);
+    }
+}
